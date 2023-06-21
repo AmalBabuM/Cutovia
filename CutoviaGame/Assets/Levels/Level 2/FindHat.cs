b2b7312e1057cfa02b2826862af00bce264fb277
Amal Babu M <104636242+AmalBabuM@users.noreply.github.com>
2023-06-21 13:48:06 +0530

    Level 2 Demo finding the hat addded

---
+++ CutoviaGame/Assets/Levels/Level 2/FindHat.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindHat : MonoBehaviour
+{
+    
+        private void Update()
+        {
+            // Check if the left mouse button is clicked
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Create a ray from the mouse position
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                // Check if the ray hits a game object
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    if(hit.collider.gameObject.tag=="Hat")
+                    {
+                        Debug.Log("Congtraz you found the hat ");
+                    }
+                    else if(hit.collider.gameObject.tag == "Cat")
+                    {
+                        Debug.Log("Its not the hat its a " + hit.collider.gameObject.tag);
+                    }
+                    else if (hit.collider.gameObject.tag == "Ball")
+                    {
+                        Debug.Log("Its not the hat its a " + hit.collider.gameObject.tag);
+                    }
+                    else if (hit.collider.gameObject.tag == "Basket")
+                    {
+                        Debug.Log("Its not the hat its a " + hit.collider.gameObject.tag);
+                    }
+                        // Display the name of the clicked game object in the console
+                       /* Debug.Log("Clicked: " + hit.collider.gameObject.name);*/
+                }
+            }
+        }
+    
+}
---
+++ CutoviaGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public enum PlayerState { idle, walking, pickup }
+
+    public PlayerState state;
+    int currentCount = 0;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+       
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+           /* Debug.Log(currentCount);*/
+            currentCount++;
+            if(currentCount > 2)
+            {
+                currentCount = 0;
+            }
+            Controll();
+        }
+       
+    }
+
+    void Controll()
+    {
+        if(currentCount==0)
+        {
+            state = PlayerState.idle;
+        }
+        else if(currentCount==1)
+        {
+            state = PlayerState.walking;
+        }
+        else if (currentCount == 2)
+        {
+            state = PlayerState.pickup;
+        }
+
+        anim.SetInteger("Status",(int)state);
+    }
+
+}
